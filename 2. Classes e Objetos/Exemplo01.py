b69410762a5a77a5bf5418603493d20aa343919e
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-05-28 01:05:40 -0300

    Armazenamento de dados em programas

---
+++ 2. Classes e Objetos/Exemplo01.py
@@ -0,0 +1,17 @@
+qtde = 2
+puni = 36.90
+ptot = qtde * puni
+msg = 'Total ='
+
+print(msg, ptot)
+# Total = 73.8
+
+# No IDLE Python:
+# qtde
+# 2
+# puni
+# 36.9
+# ptot
+# 73.8
+# msg
+# 'Total ='
