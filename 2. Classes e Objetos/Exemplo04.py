852177085f52fe9dae82d79fd5411b694056743b
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-05-30 02:44:48 -0300

    Comando de Atribuição

---
+++ 2. Classes e Objetos/Exemplo04.py
@@ -0,0 +1,19 @@
+# Os objetos só existem após serem criados com a atribuição
+# O comando de atribuição é o sinal de "="
+# print(a)      # O objeto 'a' não existe
+
+# Atribuição com informação literal, representando um conteúdo (int, str)
+a = 10
+print(a)    # 10
+
+# Atribuição com outro objeto
+b = a
+print(b)    # 10
+
+# Atribuição com fórmula matemática
+c = a + 10
+print(c)    # 20
+
+# Atribuição com função
+d = len('exemplo')
+print(d)    # 7
