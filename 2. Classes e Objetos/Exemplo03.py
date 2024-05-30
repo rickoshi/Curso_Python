6efe9c4207737039084f879d63b11a78fa1b7b5c
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-05-30 02:02:28 -0300

    Objetos de Classes Simples

---
+++ 2. Classes e Objetos/Exemplo03.py
@@ -0,0 +1,14 @@
+# Exemplos de objetos de classes simples
+n = 12
+f = 7.38
+c = 1.57 + 3.17j
+b = True
+a = False
+x = None
+
+print(type(n))  # <class 'int'>
+print(type(f))  # <class 'float'>
+print(type(c))  # <class 'complex'>
+print(type(b))  # <class 'bool'>
+print(type(a))  # <class 'bool'>
+print(type(x))  # <class 'NoneType'>
