438d97e9b4d37f2a79de6c9903db57a0a5052e85
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-05-30 01:41:25 -0300

    Classes e Objetos em Python

---
+++ 2. Classes e Objetos/Exemplo02.py
@@ -0,0 +1,12 @@
+# Classes existem para definir um gabarito para criar os objetos
+# Objeto ocupam memória e utilizamos para armazenar dados e realizar tarefas
+qtde = 2
+puni = 36.90
+ptot = qtde * puni
+msg = 'Total ='
+
+# type() exibe a classe que o objeto pertence
+type(qtde)  # <class 'int'>
+type(puni)  # <class 'float'>
+type(ptot)  # <class 'float'>
+type(msg)   # <class 'str'>
