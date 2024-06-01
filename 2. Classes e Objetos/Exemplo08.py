8f1abd540efc5235025a8da8d9d39b5a5b3f6c5a
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-06-01 08:49:05 -0300

    Operadores Aritméticos

---
+++ 2. Classes e Objetos/Exemplo08.py
@@ -0,0 +1,58 @@
+A = 14
+B = 5
+C = A + B
+print(C)        # 19
+
+C = A - B
+print(C)        # 9
+
+C = A * B
+print(C)        # 70
+
+C = A / B
+print(C)        # 2.8
+
+C = A // B
+print(C)        # 2
+
+C = A % B
+print(C)        # 4
+
+C = -A
+print(C)        # -14
+
+C = -C
+print(C)        # 14
+
+C = A ** B
+print(C)        # 537824
+
+C = 3 ** 2
+print(C)        # 9
+
+# Operações aritméticas com tipo float
+A = 2.15
+B = 5.90
+C = A + B
+print(C)        # 8.05
+
+A = 10
+B = 25
+C = 7
+R = 2 * A + B
+print(R)        # 45
+
+R = 2 * (A + B)
+print(R)        # 70
+
+X = (A + B) / (A + C)
+print(X)        # 2.0588235294117645
+
+X = (2 * B - C) / (2 * A)
+print(X)        # 2.15
+
+X = -A * 2 + B
+print(X)        # 5
+
+X = A % B * C
+print(X)        # 70
