a92ee67da1deff3995bd6d39cee742041f1c9a57
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-06-04 01:50:48 -0300

    Função print - parte 1

---
+++ 3. Comandos de Saída e Entrada/Exemplo01.py
@@ -0,0 +1,12 @@
+print('Esta é uma mensagem exemplo')    # Esta é uma mensagem exemplo
+
+X = 26
+print(X)        # 26
+Y = 58
+print(Y)        # 58
+print(X, Y)     # 26 58
+
+print('Valor de X =', X)    # Valor de X = 26
+print('Valores: X = {0} e Y = {1}'.format(X, Y))    # Valores: X = 26 e Y = 58
+print('Valores: Y = {0} e X = {1}'.format(Y, X))    # Valores: Y = 58 e X = 26
+print('Valores: X = {1} e Y = {0}'.format(Y, X))    # Valores: X = 26 e Y = 58
