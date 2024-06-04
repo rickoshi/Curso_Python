29f5dab5154fd5141aca6d8abd762dc216f18a01
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-06-04 02:04:02 -0300

    Função print - parte 2

---
+++ 3. Comandos de Saída e Entrada/Exemplo02.py
@@ -0,0 +1,22 @@
+A = 12
+B = 19
+print(A, B)                 # 12 19
+print(A, B, sep='-')        # 12-19
+print(A, B, sep=' - ')      # 12 - 19
+print(A, B, sep='<-->')     # 12<-->19
+print(A, B, sep=', ')       # 12, 19
+
+print('um\ndois\ntrês')
+# um
+# dois
+# três
+
+print('um\tdois\ttrês')     # um	dois	três
+
+X = 12
+Y = 26
+print(X)                # 12
+print(Y)                # 26
+print(X, end='...')     # 12...
+print(Y)                # 26
+# 12...26
