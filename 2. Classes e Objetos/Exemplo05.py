ae6caf52bbd498abad42e9f5da5d7deec42f893a
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-11-27 15:45:08 -0300

    Id dos objetos Python - parte 1

--- 2. Classes e Objetos/Exemplo05.py
+++ 2. Classes e Objetos/Exemplo05.py
@@ -3,16 +3,16 @@
 # Serve para associar o objeto criado
 obj1 = 10
 print(type(obj1))   # <class 'int'>
-print(id(obj1))     # 140714838923992
+print(id(obj1))     # 140730962090712
 
 obj2 = 10
 print(type(obj2))   # <class 'int'>
-print(id(obj2))     # 140714835581656
+print(id(obj2))     # 140730962090712
 # Por serem idênticos, com mesmo conteúdo e classe,
 # o Python atribui ambas as variáveis a apenas um id
 
 obj3 = 10.0
 print(type(obj3))   # <class 'float'>
-print(id(obj3))     # 1628706483312
+print(id(obj3))     # 2897932299376
 # A classe float se diferencia das outras variáveis int
 # Portanto, obj3 recebe outro id, diferente de obj1 e obj2
