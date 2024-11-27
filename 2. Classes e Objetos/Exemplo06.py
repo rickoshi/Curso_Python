9a0b639f063b30e69e8f32f39b7420af9db0bcf3
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-11-27 16:01:06 -0300

    Id dos objetos Python - parte 2

--- 2. Classes e Objetos/Exemplo06.py
+++ 2. Classes e Objetos/Exemplo06.py
@@ -15,4 +15,5 @@ print(x, y, z)  # 50 50 90
 print(id(x))    # 140714838925272
 print(id(y))    # 140714838925272
 print(id(z))    # 140714838926552
-# o objeto 'z' é imutável, pois seu id mudou
+# O objeto 'z' é imutável, pois seu id mudou
+# Quando ocorre alteração no conteúdo desse objeto, o objeto original é descartado e um novo é criado, com outro id
