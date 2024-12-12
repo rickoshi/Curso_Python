27c9f451ba7696a92b63661d8f89efdf0ff94811
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-12-12 00:54:26 -0300

    A função input - parte 2

---
+++ 3. Comandos de Saída e Entrada/Cap03_Exemplo06.py
@@ -0,0 +1,16 @@
+
+# A leitura sempre resulta em uma cadeia de caracteres (string) carregada no objeto de destino.
+# Se forem digitados apenas algarismos, ainda assim a leitura resulta em uma cadeia de caracteres.
+
+# O objeto N aparentemente recebe um número inteiro e F aparentemente recebe um número real.
+# Porém, ao utilizar o comando type, verifica-se que ambos são do tipo da classe str.
+
+N = input("Digite um inteiro: ")
+# Digite um inteiro: 37
+print(N)            # 37
+print(type(N))      # <class 'str'>
+
+F = input("Digite um real: ")
+# Digite um real: 31.47
+print(F)            # 31.47
+print(type(F))      # <class 'str'>
