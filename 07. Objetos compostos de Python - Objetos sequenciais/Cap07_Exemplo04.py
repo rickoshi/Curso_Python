b135a1c3a8a58d1accbf33dd4d18345fb7cbf4c8
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-12-22 17:58:47 -0300

    Formas de indexação

---
+++ 07. Objetos compostos de Python - Objetos sequenciais/Cap07_Exemplo04.py
@@ -0,0 +1,10 @@
+# FORMAS DE INDEXAÇÃO
+# Indexação de uma lista é o uso de um valor dentro dos colchetes para acessar um elemento individual da lista.
+# O primeiro elemento da sequência sempre terá índice 0 (erro). O índice deve sempre ser um inteiro
+# na forma literal, um objeto int, ou uma fórmula que resulte em valor inteiro.
+
+A = [10, 12, 14, 16]
+i = 0
+print(A[i])         # 10
+print(A[i+1])       # 12
+print(A[i+2])       # 14
