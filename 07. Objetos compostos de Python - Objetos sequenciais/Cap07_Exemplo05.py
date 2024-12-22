e23b7f9309229fbfe102981a60dc2eaeeb3a73ee
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-12-22 18:01:28 -0300

    Indexação com números negativos

---
+++ 07. Objetos compostos de Python - Objetos sequenciais/Cap07_Exemplo05.py
@@ -0,0 +1,8 @@
+# Em Python é possível fazer a indexação com números negativos, que permitirá o acesso aos elementos
+# de trás para frente, sendo que o índice -1 se refere ao último objeto da lista.
+
+A = [10, 12, 14, 16]
+print(A[-1])        # 16
+print(A[-2])        # 14
+print(A[-3])        # 12
+print(A[-4])        # 10
