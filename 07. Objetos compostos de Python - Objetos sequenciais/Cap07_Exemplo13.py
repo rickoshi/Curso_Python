dbbcd0db7262b977e39fc06b0fa4ccdb71ab6b36
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-12-23 17:16:27 -0300

    Uso combinado de range e for

---
+++ 07. Objetos compostos de Python - Objetos sequenciais/Cap07_Exemplo13.py
@@ -0,0 +1,23 @@
+# USO COMBINADO DE range E for
+# É muito comum usar o comando for combinado com a classe range.
+
+for i in range(4):
+    print(f'Valor i da vez = {i}')
+print('Fim do Programa')
+
+# Valor i da vez = 0
+# Valor i da vez = 1
+# Valor i da vez = 2
+# Valor i da vez = 3
+# Fim do Programa
+
+
+# Laços com while são genéricos e sempre é possível usá-los.
+# Laços com for são específicos para iterações com objetos estruturados.
+# Nem sempre é possível usar um laço for.
+# Recomendação:
+# Sempre que possível use um laço for.
+# Por quê?
+# Não haverá necessidade de usar objetos extras para controle do laço.
+# Seu código ficará mais compacto.
+# Seu código ficará mais legível.
