1e6bae710e598d0522072089b15ef8043f8b6918
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-12-23 17:22:07 -0300

    Multiplicação com "*"

---
+++ 07. Objetos compostos de Python - Objetos sequenciais/Cap07_Exemplo15.py
@@ -0,0 +1,9 @@
+# MULTIPLICAÇÃO COM "*"
+# O operador de multiplicação "*" serve para criar sequências com múltiplas cópias de uma subsequência.
+# Ele é particularmente útil nas situações em que precisamos de uma lista com uma certa
+# quantidade pré-definida de elementos e carregados com um determinado valor inicial.
+
+A = [1, 2, 3] * 3
+print(A)            # [1, 2, 3, 1, 2, 3, 1, 2, 3]
+B = [0] * 10
+print(B)            # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
