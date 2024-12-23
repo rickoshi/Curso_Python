bc523fd6c6bd214bf6bc97cb070bdb424f868ccd
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-12-23 17:19:43 -0300

    Concatenação com "+"

---
+++ 07. Objetos compostos de Python - Objetos sequenciais/Cap07_Exemplo14.py
@@ -0,0 +1,12 @@
+# OPERADORES DE CONCATENAÇÃO E MULTIPLICAÇÃO APLICADO A CLASSES SEQUENCIAIS
+# Vamos apresentar dois operadores muito simples e práticos que podem ser usados com listas, tuplas e strings.
+
+# CONCATENAÇÃO COM "+"
+# Neste exemplo são definidas duas listas A e B, e em seguida foi usado o operador "+" para juntar as duas listas.
+# Assim, no contexto de classes sequenciais o operador "+" é denominado "operador de concatenação".
+# Ele também pode ser usado com tuplas e strings.
+
+A = [10, 12, 14, 16]
+B = [20, 22, 24]
+R = A + B       # # operador de concatenação '+' usado para juntar duas listas
+print(R)        # [10, 12, 14, 16, 20, 22, 24]
