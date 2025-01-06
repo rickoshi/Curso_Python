e619c40ed200afcef87ca06df648237a605072b4
rickoshi <145235121+rickoshi@users.noreply.github.com>
2025-01-06 02:29:48 -0300

    Comando match-case

---
+++ 08. Comandos Condicionais - Aprofundamento/Cap08_Exemplo02.py
@@ -0,0 +1,18 @@
+# Este comando é bastante flexível com relação as verificações de correspondência que podem ser feitas.
+# O exemplo a seguir ilustra outras possibilidades.
+
+# entrada = ObtemEntrada()
+# match entrada:
+#     case 1:
+#         print(' a entrada é 1')
+#     case 'texto':
+#         print(' a entrada é um texto')
+#     case [a, b]:
+#         print(' a entrada é uma lista com dois elementos')
+#     case _:
+#         print(' a entrada é uma outra opção')
+
+# este exemplo apenas ilustra o comando match-case.
+# neste momento não pode ser executado pois ainda não temos os elementos
+# necessários para implementar a função ObtemEntrada().
+# Mais adiante neste texto vamos aprender a criar funções como essa
